fdffd4d4e957013388047f9dc5b210cd526ef48c
= <=>
2023-03-03 02:24:30 +0500

    Product attributes module

---
+++ database/migrations/2023_03_02_204454_create_product_attributes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('size')->nullable();
+            $table->float('price')->nullable();
+            $table->integer('stock')->nullable();
+            $table->string('sku')->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_attributes', function (Blueprint $table) {
+            $table->dropForeign(['product_id']);
+        });
+        Schema::dropIfExists('product_attributes');
+    }
+}
