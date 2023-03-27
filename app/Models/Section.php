a756755f8e0d415aba6764bcb749c97abe5e04ba
= <=>
2023-03-28 01:42:19 +0500

    Made Navbar and Sidebar Dynamic

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Section extends Model
 {
+    // // For Front End
+    // public function sections(){
+        
+    //     $sections = Section::where('status', 1)->get();
+
+    //     return $sections;
+    // }
+
     public function categories(){
         return $this->hasMany(Category::class, 'section_id')->where(['parent_id'=>0, 'status'=>1]);
     }
