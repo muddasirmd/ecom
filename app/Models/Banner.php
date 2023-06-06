79c3fada9a2bc208eddb0d471ab04f8631fdb4a5
= <=>
2023-06-07 00:36:14 +0500

    Banners Module

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     protected $guarded = ['id'];
+
+    public static function getBanners(){
+        $banners = Banner::where("status", 1)->get();
+        return $banners;
+    }
 }
--- resources/views/layouts/front/front_layout.blade.php
+++ resources/views/layouts/front/front_layout.blade.php
@@ -26,44 +26,9 @@
 <body>
 @include('layouts.front.front_header')
 <!-- Header End====================================================================== -->
-@if(isset($page_name) && $page_name == 'index')
-	<div id="carouselBlk">
-		<div id="myCarousel" class="carousel slide">
-			<div class="carousel-inner">
-				<div class="item active">
-					<div class="container">
-						<a href="#"><img style="width:100%" src="{{ asset('images/front/carousel/1.png') }}" alt="special offers"/></a>
-						<div class="carousel-caption">
-							<h4>First Thumbnail label</h4>
-							<p>Banner text</p>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="container">
-						<a href="register.html"><img style="width:100%" src="{{ asset('images/front/carousel/2.png') }}" alt=""/></a>
-						<div class="carousel-caption">
-							<h4>Second Thumbnail label</h4>
-							<p>Banner text</p>
-						</div>
-					</div>
-				</div>
-				<div class="item">
-					<div class="container">
-						<a href="register.html"><img src="{{ asset('images/front/carousel/3.png') }}" alt=""/></a>
-						<div class="carousel-caption">
-							<h4>Third Thumbnail label</h4>
-							<p>Banner text</p>
-						</div>
-						
-					</div>
-				</div>
-			</div>
-			<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-			<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-		</div>
-	</div>
-@endif
+
+@include('front.home_page_banners')
+
 <div id="mainBody">
 	<div class="container">
 		<div class="row">
